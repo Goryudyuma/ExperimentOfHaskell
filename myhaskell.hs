e82ea7fb621bc406c696e34da7079236e012cc4a
Goryudyuma <umaretekyoumade@gmail.com>
2014-09-24 22:36:47 +0900

    test

--- myhaskell.hs
+++ myhaskell.hs
@@ -22,20 +22,15 @@ infixr 5 /-:-
 (/-:-) :: Fraction -> Fraction -> Fraction
 (Fraction a (b,c)) /-:- (Fraction x (y,z)) = reduce ((a-x) /- (b*z - c*y, c*z))
 
-<<<<<<< HEAD
 -- Fraction Int (Int, Int)“¯m‚ÌŠ|‚¯Z
-<<<<<<< HEAD
 infixr 5 ++*
 (++*) :: Fraction Int (Int, Int) -> Fraction Int (Int, Int) -> Fraction Int (Int, Int)
 (Fraction a (b,c)) ++* (Fraction x (y,z)) = reduce (0 /- (a*c*x*z+b*x*z+a*c*y+b*y, c*z))
-=======
-=======
+
 -- Fraction“¯m‚ÌŠ|‚¯Z
->>>>>>> 2aaab91815bec166d1dcf8689bb5e4ac2ef73957
 infixr 6 /-:*
 (/-:*) :: Fraction -> Fraction -> Fraction
 (Fraction a (b,c)) /-:* (Fraction x (y,z)) = reduce (0 /- (a*c*x*z+b*x*z+a*c*y+b*y, c*z))
->>>>>>> 104a7ddc03c44e066957bf1d4bf00c2c77bcebeb
 
 
 -- –ñ•ª‚·‚éŠÖ”
