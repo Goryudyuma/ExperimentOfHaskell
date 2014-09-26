4c8eba91973785dc3a38c4ca5783295835423f28
MichaeGon <lodnix@gmail.com>
2014-09-26 21:21:00 +0900

    revert

--- Fraction.hs
+++ Fraction.hs
@@ -40,50 +40,71 @@ x /- (y,z)
 -- Fraction“¯m‚Ì‘«‚µZ
 infixr 5 /-:+
 (/-:+) :: Fraction -> Fraction -> Fraction
+<<<<<<< HEAD
 (Fraction a (b,c)) /-:+ (Fraction x (y,z)) = reduce $ (a+x) /- (b*z + c*y, c*z)
 _ /-:+ _ = Invalid
+=======
+Invalid /-:+ _ = Invalid
+_ /-:+ Invalid = Invalid
+(Fraction a (b,c)) /-:+ (Fraction x (y,z)) = reduce ((a+x) /- (b*z + c*y, c*z))
+>>>>>>> parent of c78fbb4... é–¢æ•°ã®ãƒ‘ã‚¿ãƒ¼ãƒ³ãƒãƒƒãƒã®ç°¡ç•¥åŒ–
 
 -- Fraction“¯m‚Ìˆø‚«Z
 infixr 5 /-:-
 (/-:-) :: Fraction -> Fraction -> Fraction
+<<<<<<< HEAD
 (Fraction a (b,c)) /-:- (Fraction x (y,z)) = reduce $ (a-x) /- (b*z - c*y, c*z)
 _ /-:- _ = Invalid
+=======
+Invalid /-:- _ = Invalid
+_ /-:- Invalid = Invalid
+(Fraction a (b,c)) /-:- (Fraction x (y,z)) = reduce ((a-x) /- (b*z - c*y, c*z))
+>>>>>>> parent of c78fbb4... é–¢æ•°ã®ãƒ‘ã‚¿ãƒ¼ãƒ³ãƒãƒƒãƒã®ç°¡ç•¥åŒ–
 
 -- Fraction“¯m‚ÌŠ|‚¯Z
 infixr 6 /-:*
 (/-:*) :: Fraction -> Fraction -> Fraction
+Invalid /-:* _ = Invalid
+_ /-:* Invalid = Invalid
 (Fraction a (b,c)) /-:* (Fraction x (y,z))
 	| a == 0 && x == 0 = reduce (0 /- (b*y, c*z))
 	| a /= 0 = toImproper (Fraction a (b, c)) /-:* (Fraction x (y, z))
 	| otherwise = (Fraction a (b, c)) /-:* (toImproper $ Fraction x (y, z))
-_ /-:* _ = Invalid
 
 -- Fraction“¯m‚ÌŠ„‚èZ
 infixr 6 /-:/
 (/-:/) :: Fraction -> Fraction -> Fraction
+<<<<<<< HEAD
 (Fraction a (b,c)) /-:/ (Fraction x (y,z)) = (Fraction a (b,c)) /-:* toReverse $ Fraction x (y,z)
 _ /-:/ _ = Invalid
+=======
+Invalid /-:/ _ = Invalid
+_ /-:/ Invalid = Invalid
+(Fraction a (b,c)) /-:/ (Fraction x (y,z)) = (Fraction a (b,c)) /-:* toReverse (Fraction x (y,z))
+>>>>>>> parent of c78fbb4... é–¢æ•°ã®ãƒ‘ã‚¿ãƒ¼ãƒ³ãƒãƒƒãƒã®ç°¡ç•¥åŒ–
 
 -- –ñ•ª‚·‚éŠÖ”
 reduce :: Fraction -> Fraction
+reduce Invalid = Invalid
 reduce (Fraction a (b,c)) = a /- (b `div` (gcd b c), c `div` (gcd b c))
-reduce _ = Invalid
 
 -- Double‚É•ÏŠ·‚·‚éŠÖ”
 toDouble :: Fraction -> Maybe Double
+toDouble Invalid = Nothing
 toDouble (Fraction x (y, z)) = Just $ fromIntegral (x*z + y) / fromIntegral z
-toDouble _ = Nothing
 
 --‰¼•ª”(improper fractions)‚É‚·‚éŠÖ”
 toImproper :: Fraction -> Fraction
+toImproper Invalid = Invalid
 toImproper (Fraction x (y, z)) = Fraction 0 (x*z+y, z)
-toImproper _ = Invalid
 
 --y‚Æz‚ğ‚Ğ‚Á‚­‚è•Ô‚·ŠÖ”B
 toReverse :: Fraction -> Fraction
+toReverse Invalid = Invalid
 toReverse (Fraction x (y, z)) 
 	| x == 0 = Fraction x (z, y)
 	| otherwise = toReverse $ toImproper $ Fraction x (y, z)
+<<<<<<< HEAD
 toReverse _ = Invalid
 
 --®”•”•ª‚ğw’è‚µ‚ÄŒvZŒ‹‰Ê‚ğ•Ô‚·ŠÖ”B
@@ -92,3 +113,5 @@ assign' a (Fraction x (y,z)) = Fraction a ((x-a)*z+y,z)
 assign' _ _ = Invalid
 
 
+=======
+>>>>>>> parent of c78fbb4... é–¢æ•°ã®ãƒ‘ã‚¿ãƒ¼ãƒ³ãƒãƒƒãƒã®ç°¡ç•¥åŒ–
