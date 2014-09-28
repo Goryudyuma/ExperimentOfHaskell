9f226e9f3936a216bdc2092820f270fe972d87a3
MichaeGon <lodnix@gmail.com>
2014-09-28 18:32:30 +0900

    ファイル名選択可能に

--- helloworld.hs
+++ helloworld.hs
@@ -1,7 +1,13 @@
 {-# OPTIONS -Wall -Werror #-}
 
+import System.Environment
+
 main :: IO ()
-main = writeFile "helloworld.c" helloC
+main = do
+	args <- getArgs
+	flip writeFile helloC $ case args of
+		[] -> "helloworld.c"
+		_ -> head args ++ ".c"
 
 helloC :: String
 helloC = "#include <stdio.h>\nint main(void) {\n\tprintf(\"Hello, world\\n\");\n\treturn 0;\n}"
