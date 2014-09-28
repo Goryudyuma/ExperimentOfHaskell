bf9676c54bca6f4a377dc7f8f1e545d4a201493f
MichaeGon <lodnix@gmail.com>
2014-09-28 18:22:53 +0900

    セミコロン懐かしい

--- helloworld.hs
+++ helloworld.hs
@@ -4,4 +4,4 @@ main :: IO ()
 main = writeFile "helloworld.c" helloC
 
 helloC :: String
-helloC = "#include <stdio.h>\nint main(void) {\n\tprintf(\"Hello, world\\n\")\n\treturn 0\n}"
+helloC = "#include <stdio.h>\nint main(void) {\n\tprintf(\"Hello, world\\n\");\n\treturn 0;\n}"
