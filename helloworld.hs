2f3bd6d58923ac3bea5f7943f3b6b323c890b576
MichaeGon <lodnix@gmail.com>
2014-09-28 18:20:53 +0900

    フィアル出力テスト

---
+++ helloworld.hs
@@ -0,0 +1,7 @@
+{-# OPTIONS -Wall -Werror #-}
+
+main :: IO ()
+main = writeFile "helloworld.c" helloC
+
+helloC :: String
+helloC = "#include <stdio.h>\nint main(void) {\n\tprintf(\"Hello, world\\n\")\n\treturn 0\n}"
